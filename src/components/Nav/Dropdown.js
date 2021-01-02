55fd650f80596f3df4630c0b3cd1c255816f7663
Miley Wright <dishaliekwright@yahoo.com>
2021-01-02 14:31:16 -0500

    nav dropdown

--- src/components/Nav/Dropdown.js
+++ src/components/Nav/Dropdown.js
@@ -8,13 +8,23 @@ export const apparel = (
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/anime-tee-shirts">
+          Tee Shirts
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/anime-hoodies-and-sweatshirts">
+          Hoodies
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/anime-socks">
+          Socks
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/anime-weeb-cosplay-accessories">
+          Cosplay
         </Link>
       </Menu.Item>
     </Menu>
@@ -23,18 +33,18 @@ export const apparel = (
   export const homegoods = (
     <Menu>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/anime-homegoods">
+          All Homegoods
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/3d-led-anime-bedroom-lights-lamps">
+          3D LED Lights
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/anime-kawaii-cartoon-bedroom-pillows">
+          Pillows
         </Link>
       </Menu.Item>
     </Menu>
@@ -42,18 +52,38 @@ export const apparel = (
   export const lifestyle = (
     <Menu>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/sad-aesthetic">
+          Sad Aesthetic
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/vaporwave-aesthetic-clothing-tees-hoodies-merch">
+          Vaporwave
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/nsfw-anime-merch">
+          NSFW
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/kawaii-livestyle-accessories">
+          Kawaii
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/japanese-senpai-shirts-and-hoodies">
+          Senpai
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/japanese-waifu-shirts-and-hoodies">
+          Waifu
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/anime-cosplay-accessories">
+          Cosplay
         </Link>
       </Menu.Item>
     </Menu>
@@ -62,18 +92,13 @@ export const apparel = (
   export const techAccessories = (
     <Menu>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/anime-phone-cases">
+          Phone Cases
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
-        </Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/anime-airpod-1-2-pro-cases">
+          AirPod Cases
         </Link>
       </Menu.Item>
     </Menu>
@@ -82,18 +107,58 @@ export const apparel = (
   export const byAnime = (
     <Menu>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/all-anime-merch">
+          All Anime Merch
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/attack-on-titan-anime-merch">
+          Attack On Titan
         </Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/collection/anime-clothing-apparel">
-          All Apparel
+        <Link to="/collection/darling-in-the-franxx-anime-weeb-merch">
+          Darling In The Franxx
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/demon-slayer-anime-merch">
+          Demon Slayer
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/dragon-ball-z-anime-merch">
+          Dragon Ball Z
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/hunter-x-hunter-anime-merch">
+          Hunter X Hunter
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/my-hero-academia-anime-merch">
+          My Hero Academia
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/my-neighbor-totoro-anime-merch">
+          My Neighbor Totoro
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/naruto-anime-merch">
+          Naruto
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/one-piece-anime-merch">
+          One Piece
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/collection/sailor-moon-anime-merch">
+          Sailor Moon
         </Link>
       </Menu.Item>
     </Menu>
