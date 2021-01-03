e98cb5f690b576b1208307130baeef5c7a740c1a
Miley Wright <dishaliekwright@yahoo.com>
2021-01-03 02:33:43 -0500

    apparel merch

--- src/components/Apparel/Apparel.js
+++ src/components/Apparel/Apparel.js
@@ -24,20 +24,25 @@ const Apparel = () => {
     return (
         <>
             <Nav/>
-            <div className='title'> Apparel</div>
-            <div className='container'>
-                {apparel && apparel.length > 0 &&
-                apparel.slice(minValue, maxValue).map(item => <Link to={`/collections/anime-clothing-apparel/products/${item.id}`}><Card item={item} key={item.id}/></Link>)}
+            <div className='product_page'>
+                <div className='title'> Apparel</div>
+                <div className='container'>
+                    {apparel && apparel.length > 0 &&
+                    apparel.slice(minValue, maxValue).map(item => 
+                        <Link to={`/collections/anime-clothing-apparel/products/${item.id}`} className='link overlay'>
+                            <Card className='product_card 'item={item} key={item.id}/>
+                        </Link>
+                    )}
+                </div>
+                <Pagination
+                    className='pagination'
+                    defaultCurrent={1}
+                    defaultPageSize={numEachPage}
+                    showSizeChanger={false}
+                    onChange={handleChange}
+                    total={apparel.length}
+                />
             </div>
-            <Pagination
-                className='pagination'
-                defaultCurrent={1}
-                defaultPageSize={numEachPage}
-                showSizeChanger={false}
-                onChange={handleChange}
-                total={apparel.length}
-            />
-           
             <Footer/>
         </>
     )
--- src/components/Products/Card.js
+++ src/components/Products/Card.js
@@ -1,9 +1,12 @@
 import './Card.css';
 
 const Card = (props) => {
+    
     return(
         <div className='card'>
             <img src={props.item.photo} alt={props.item.name}/>
+            <p>{props.item.name}</p>
+            <p>${props.item.price}</p>
         </div>
     )
 }
