b7aa311c54f18ec6764f4d5df5cf46184c7440de
Miley Wright <dishaliekwright@yahoo.com>
2021-01-02 21:15:21 -0500

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,10 @@
 import './Footer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShippingFast } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShippingFast } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return(
@@ -13,7 +17,27 @@ const Footer = () => {
             </div>
             
             <div className='footer'>
-                
+                <div className='grid_list'>
+                    <div className='box_item'>Quick Links</div>
+                    <Link to='/shipping-and-returns-refund-policy' className='box_item link'>Shipping Info</Link>
+                    <Link to='/pages/contact-us' className='box_item link'>Contact Us</Link>
+                    <Link to='/policies/refund-policy' className='box_item link'>Refund Policy</Link>
+                    <Link to='/policies/privacy-policy' className='box_item link'>Privacy Policy</Link>
+                    <Link to='/policies/terms-of-service' className='box_item link'>Terms of Service</Link>
+                </div>
+                <div className='grid_list'>
+                    <div className='box_item'>Follow Us</div>
+                    <Link to='/' className='box_item link'> <FontAwesomeIcon icon={faFacebookF} className='icon' /> Facebook</Link>
+                    <Link to='/' className='box_item link'><FontAwesomeIcon icon={faInstagram} className='icon' /> Instagram</Link>
+                </div>
+                <div className='grid_list'>
+                    <div className='box_item'>Subscribe To Our Newsletter</div>
+                    <p>We'll only send you coupons and cool stuff. We swear.</p>
+                </div>
+                <div className='grid_list'>
+                    <div className='box_item'>Contact Us</div>
+                    <p>Hit us up!</p>
+                </div>
             </div>
         </>
     )
